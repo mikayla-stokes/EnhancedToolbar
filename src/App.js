d383516b8210e4273310edded47ad28abe1648c7
mikayla-stokes <mikaylastokes@usf.edu>
2024-07-14 22:43:31 -0400

    Add dynamic movie selector to toolbar.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RateMovieButton from './components/RateMovieButton';
 import './App.css';
 
+const movies = ['Movie 1', 'Movie 2', 'Movie 3'];
+
 function App() {
+  const [selectedMovie, setSelectedMovie] = useState(movies[0]);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Interactive Media Toolbar</h1>
         <div className="toolbar">
-          <RateMovieButton movie="Example Movie" />
+          <select 
+            value={selectedMovie} 
+            onChange={(e) => setSelectedMovie(e.target.value)}
+          >
+            {movies.map((movie) => (
+              <option key={movie} value={movie}>{movie}</option>
+            ))}
+          </select>
+          <RateMovieButton movie={selectedMovie} />
         </div>
       </header>
     </div>
